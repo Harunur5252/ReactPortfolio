64c3f0f3f08827b31766f2b2e16ce6d2dd0da084
Harunur5252 <hroshid695@gmil.com>
2022-12-10 23:12:42 +0600

    fetched data for privacy,refund and termss page

--- src/components/RefandDescription/RefandDes.js
+++ src/components/RefandDescription/RefandDes.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { Component, Fragment } from 'react';
 import {Col, Container, Row} from "react-bootstrap";
 import ReactMarkdown from 'react-markdown'
+import Loading from '../Loading/Loading';
 
 class RefandDes extends Component {
     state = {
@@ -24,19 +25,36 @@ class RefandDes extends Component {
         }
        }
     render() {
+        if(this.state.loading){
             return (
-            <Fragment>
-                 <Container className="mt-5">
-                    <Row>
-                       <Col sm={12} md={12} lg={12}>
-                           <p className="text-justify">
-                              <ReactMarkdown>{this.state?.refandPolicy}</ReactMarkdown>
-                            </p>
+             <Fragment>
+             <Container fluid className="p-0">
+                 <Container className="topContent text-center">
+                     <Row>
+                       <Col>
+                           <Loading />
                        </Col>
-                    </Row>
-                </Container>
-            </Fragment>
-        );
+                     </Row>
+                 </Container>
+             </Container>
+           </Fragment>
+            )
+        }else{
+            return (
+                <Fragment>
+                     <Container className="mt-5">
+                        <Row>
+                           <Col sm={12} md={12} lg={12}>
+                               <p className="text-justify">
+                                  <ReactMarkdown>{this.state?.refandPolicy}</ReactMarkdown>
+                                </p>
+                           </Col>
+                        </Row>
+                    </Container>
+                </Fragment>
+                );
+        }
+            
         }
         
     }
--- src/components/TermsDescription/TermsDes.js
+++ src/components/TermsDescription/TermsDes.js
@@ -1,21 +1,65 @@
+import axios from 'axios';
 import React, { Component,Fragment } from 'react';
 import {Col, Container, Row} from "react-bootstrap";
+import ReactMarkdown from 'react-markdown'
+import Loading from '../Loading/Loading';
+
 class TermsDes extends Component {
-    render() {
-            return (
-            <Fragment>
-                 <Container className="mt-5">
-                    <Row>
-                       <Col sm={12} md={12} lg={12}>
-                       <p className="serviceDescription text-justify">BY ACCESSING OR USING THIS SITE YOU REPRESENT THAT YOU HAVE THE FULL AUTHORITY TO ACT TO BIND YOURSELF, ANY THIRD PARTY, COMPANY, OR LEGAL ENTITY, AND THAT YOUR USE AND/OR INTERACTION, AS WELL AS CONTINUING TO USE OR INTERACT, WITH THE SITE CONSTITUTES YOUR HAVING READ AND AGREED TO THESE TERMS OF USE AS WELL AS OTHER AGREEMENTS THAT WE MAY POST ON THE SITE.</p>
-                        <p className="serviceDescription text-justify">BY ACCESSING OR USING THIS SITE YOU REPRESENT THAT YOU HAVE THE FULL AUTHORITY TO ACT TO BIND YOURSELF, ANY THIRD PARTY, COMPANY, OR LEGAL ENTITY, AND THAT YOUR USE AND/OR INTERACTION, AS WELL AS CONTINUING TO USE OR INTERACT, WITH THE SITE CONSTITUTES YOUR HAVING READ AND AGREED TO THESE TERMS OF USE AS WELL AS OTHER AGREEMENTS THAT WE MAY POST ON THE SITE.</p>
-                           
-                       </Col>
-                    </Row>
-                </Container>
-            </Fragment>
-        );
+    state = {
+        loading :true,
+      }
+    
+      componentDidMount(){
+        this.fetchTermsConditionData()
+      }
+       fetchTermsConditionData = async () => {
+        try {
+           const res = await axios.get('http://localhost:1337/api/terms-and-condition')
+           const data = res.data?.data?.attributes
+           this.setState({
+              condition:data?.condition,
+              loading : false
+           })
+        } catch (error) {
+          console.log(error.response)
         }
+       }
+
+    render() {
+        if(this.state.loading){
+            return (
+             <Fragment>
+             <Container fluid className="p-0">
+                 <Container className="topContent text-center">
+                     <Row>
+                       <Col>
+                           <Loading />
+                       </Col>
+                     </Row>
+                 </Container>
+             </Container>
+           </Fragment>
+            )
+        }else{
+            return (
+                <Fragment>
+                     <Container className="mt-5">
+                        <Row>
+                           <Col sm={12} md={12} lg={12}>
+                           <p className="serviceDescription text-justify">
+                              <ReactMarkdown>
+                                  {this.state?.condition}
+                              </ReactMarkdown>
+                           </p>
+                               
+                           </Col>
+                        </Row>
+                    </Container>
+                </Fragment>
+               );
+        }
+           
+    }
 }
 
 export default TermsDes;
