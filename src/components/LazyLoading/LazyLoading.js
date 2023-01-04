49dbbe5cd53644cba3d516d3a044f3164ce58060
Harunur5252 <hroshid695@gmil.com>
2023-01-04 21:22:06 +0600

    LazyLoading component add TopNavigation and Footer component for better display

--- src/components/LazyLoading/LazyLoading.js
+++ src/components/LazyLoading/LazyLoading.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 import { ScaleLoader } from 'react-spinners'
+import TopNavigation from '../TopNavigation/TopNavigation'
+import Footer from '../Footer/Footer'
 
 export default class LazyLoading extends Component {
   render() {
     return (
-      <div style={{margin:'0 auto',marginTop:'12rem',textAlign:'center'}}>
-         <ScaleLoader height={40} width={8} speedMultiplier={2} color="#36d7b7" />
-      </div>
+     <>
+          <TopNavigation />
+          <div style={{margin:'0 auto',marginTop:'12rem',textAlign:'center'}}>
+            <ScaleLoader height={40} width={8} speedMultiplier={2} color="#36d7b7" />
+          </div>
+          <div style={{marginTop:'10rem'}}>
+             <Footer />
+          </div>
+     </>
     )
   }
 }
